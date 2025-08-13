22e26098d61ce383918a4601c0d61f334ff0cefa
Pradyot Srivastava <pradyotsrivastava93@gmail.com>
2025-08-13 22:31:51 +0530

    updates

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -42,6 +42,7 @@ const Login = () => {
     const dataApi = await dataResponse.json();
 
     if (dataApi.success) {
+      console.log("Working Properly");
       await fetchUserDetails();
       await fetchUserAddToCart();
       navigate("/");
